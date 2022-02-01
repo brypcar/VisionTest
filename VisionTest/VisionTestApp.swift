a57f58be1140aafd439100ce15b2dc378d1c0f98
BryanCarey <brypcar@icloud.com>
2022-02-01 12:45:04 -0500

    Initial Commit

---
+++ VisionTest/VisionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionTestApp.swift
+//  VisionTest
+//
+//  Created by Bryan Carey on 2/1/22.
+//
+
+import SwiftUI
+
+@main
+struct VisionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
